5a0b4bc31b2606dcfece912390be414c641d4d9c
kaldius <quantf@gmail.com>
2021-10-07 16:46:25 +0800

    Refactor constant strings in Messages from "person" to "event"

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -71,7 +71,7 @@ public class EditCommand extends Command {
         List<Event> lastShownList = model.getFilteredEventList();
 
         if (index.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            throw new CommandException(Messages.MESSAGE_INVALID_EVENT_DISPLAYED_INDEX);
         }
 
         Event personToEdit = lastShownList.get(index.getZeroBased());
