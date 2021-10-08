820fd88f473eedba470dee01b65b49c0a614ad0d
Galvin <e0543848@u.nus.edu>
2021-10-08 21:49:45 +0800

    Change "address" to "location" in Location

--- src/main/java/seedu/address/model/person/Location.java
+++ src/main/java/seedu/address/model/person/Location.java
@@ -4,15 +4,15 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
 /**
- * Represents a Person's address in the address book.
+ * Represents a Person's location in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidLocation(String)}
  */
 public class Location {
 
-    public static final String MESSAGE_CONSTRAINTS = "Addresses can take any values, and it should not be blank";
+    public static final String MESSAGE_CONSTRAINTS = "Locations can take any values, and it should not be blank";
 
     /*
-     * The first character of the address must not be a whitespace,
+     * The first character of the location must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
     public static final String VALIDATION_REGEX = "[^\\s].*";
@@ -22,16 +22,16 @@ public class Location {
     /**
      * Constructs an {@code Location}.
      *
-     * @param address A valid address.
+     * @param location A valid location.
      */
-    public Location(String address) {
-        requireNonNull(address);
-        checkArgument(isValidLocation(address), MESSAGE_CONSTRAINTS);
-        value = address;
+    public Location(String location) {
+        requireNonNull(location);
+        checkArgument(isValidLocation(location), MESSAGE_CONSTRAINTS);
+        value = location;
     }
 
     /**
-     * Returns true if a given string is a valid email.
+     * Returns true if a given string is a valid location.
      */
     public static boolean isValidLocation(String test) {
         return test.matches(VALIDATION_REGEX);
