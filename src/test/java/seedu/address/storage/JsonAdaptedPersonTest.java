3d849d9b3c83be791f822c772e8b0bb1a2f6d0a0
Ruth <iluvcelebi@gmail.com>
2021-10-09 21:34:40 +0800

    Passes java checkstyle:Test

--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -13,9 +13,9 @@ import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
+import seedu.address.model.person.Date;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
-import seedu.address.model.person.Date;
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
