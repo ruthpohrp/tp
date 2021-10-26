6e69702dce0f69507893d6a8ad17996b5c4f93df
Ruth <iluvcelebi@gmail.com>
2021-10-27 00:37:25 +0800

    Update SortedBlockedSlotList Tests

--- src/test/java/seedu/address/testutil/TypicalBlockedSlots.java
+++ src/test/java/seedu/address/testutil/TypicalBlockedSlots.java
@@ -7,11 +7,14 @@ import seedu.address.model.event.BlockedSlot;
  */
 public class TypicalBlockedSlots {
 
+    public static final BlockedSlot EIGHT_TO_NINE = new BlockedSlotBuilder()
+            .withTimeSlot("0800", "0900").withDate("2020-01-01").build();
+
     public static final BlockedSlot NINE_TO_TEN = new BlockedSlotBuilder()
             .withTimeSlot("0900", "1000").withDate("2020-01-01").build();
 
-    public static final BlockedSlot EIGHT_TO_NINE = new BlockedSlotBuilder()
-            .withTimeSlot("0800", "0900").withDate("2020-01-01").build();
+    public static final BlockedSlot EIGHT_TO_NINE_02 = new BlockedSlotBuilder()
+            .withTimeSlot("0800", "0900").withDate("2020-02-02").build();
 
 
     private TypicalBlockedSlots() {
