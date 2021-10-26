4c76daec14f44cf3ced372b024dc2945103103bd
kaldius <quantf@gmail.com>
2021-10-26 15:07:44 +0800

    Create BlockedSlot which implements Overlappable

---
+++ src/main/java/seedu/address/model/event/BlockedSlot.java
@@ -0,0 +1,27 @@
+package seedu.address.model.event;
+
+import static java.util.Objects.requireNonNull;
+
+/**
+ * Represents a blocked period of time which an Event should not overlap with.
+ */
+public class BlockedSlot implements Overlappable {
+
+    public static final String MESSAGE_CONSTRAINTS = "Blocked time should be of the format HHmm-HHmm e.g 1300-1400. ";
+
+    public final Date date;
+    public final TimeSlot timeSlot;
+
+    public BlockedSlot(Date date, TimeSlot timeSlot) {
+        requireNonNull(date);
+        requireNonNull(timeSlot);
+        this.date = date;
+        this.timeSlot = timeSlot;
+    }
+
+    @Override
+    public boolean overlaps(Overlappable overlappable) {
+        //TODO: For Ruth to implement.
+        return false;
+    }
+}
--- src/main/java/seedu/address/model/event/Overlappable.java
+++ src/main/java/seedu/address/model/event/Overlappable.java
@@ -1,5 +1,8 @@
 package seedu.address.model.event;
 
+/**
+ * An Overlappable is an object that can overlap with another based on date and time.
+ */
 public interface Overlappable {
 
     boolean overlaps(Overlappable overlappable);
