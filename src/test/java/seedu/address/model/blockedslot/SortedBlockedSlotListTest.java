2632ed6b685741e036110da5da1be36b7563499a
kaldius <quantf@gmail.com>
2021-10-27 17:46:18 +0800

    Create blockedslot package for test cases

--- src/test/java/seedu/address/model/blockedslot/SortedBlockedSlotListTest.java
+++ src/test/java/seedu/address/model/blockedslot/SortedBlockedSlotListTest.java
@@ -1,4 +1,4 @@
-package seedu.address.model.event;
+package seedu.address.model.blockedslot;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
