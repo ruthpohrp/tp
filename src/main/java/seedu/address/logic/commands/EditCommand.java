8ae3fd5e508b877711dc1b6e624d023831d436d4
kaldius <quantf@gmail.com>
2021-11-05 09:45:06 +0800

    Add check for overlapping events in EditCommand

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -19,7 +19,9 @@ import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.CollectionUtil;
 import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.logic.commands.exceptions.SlotBlockedException;
 import seedu.address.model.Model;
+import seedu.address.model.blockedslot.BlockedSlot;
 import seedu.address.model.event.Date;
 import seedu.address.model.event.Event;
 import seedu.address.model.event.Location;
@@ -78,6 +80,12 @@ public class EditCommand extends Command {
 
         Event eventToEdit = lastShownList.get(index.getZeroBased());
         Event editedEvent = createEditedEvent(eventToEdit, editEventDescriptor);
+
+        if (model.isBlockedByBlockedSlot(editedEvent)) {
+            throw new SlotBlockedException(BlockedSlot.SLOT_BLOCKED);
+        } else if (model.isBlockedByEvent(editedEvent)) {
+            throw new SlotBlockedException(Event.SLOT_BLOCKED);
+        }
 
         model.setEvent(eventToEdit, editedEvent);
         model.updateFilteredEventList(PREDICATE_SHOW_ALL_EVENTS);
