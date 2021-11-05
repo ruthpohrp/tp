b51a29f3fa900905618caccf0ab83ed5ddc68981
kaldius <quantf@gmail.com>
2021-11-05 16:40:35 +0800

    Add testing for storage

--- src/test/java/seedu/address/storage/JsonSerializableScheduleTest.java
+++ src/test/java/seedu/address/storage/JsonSerializableScheduleTest.java
@@ -18,6 +18,10 @@ public class JsonSerializableScheduleTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableScheduleTest");
     private static final Path TYPICAL_EVENTS_FILE = TEST_DATA_FOLDER.resolve("typicalEventSchedule.json");
     private static final Path INVALID_EVENT_FILE = TEST_DATA_FOLDER.resolve("invalidEventSchedule.json");
+    private static final Path INVALID_BLOCKEDSLOT_FILE = TEST_DATA_FOLDER.resolve("invalidBlockedSlotSchedule.json");
+    private static final Path CLASHING_EVENT_FILE = TEST_DATA_FOLDER.resolve("clashingEventSchedule.json");
+    private static final Path CLASHING_BLOCKEDSLOT_FILE = TEST_DATA_FOLDER.resolve("clashingBlockedSlotSchedule.json");
+    private static final Path EVENT_CLASHES_WITH_BLOCKEDSLOT_FILE = TEST_DATA_FOLDER.resolve("eventClashesWithBlockedSlotSchedule.json");
 
     @Test
     public void toModelType_typicalEventsFile_success() throws Exception {
@@ -31,6 +35,34 @@ public class JsonSerializableScheduleTest {
     @Test
     public void toModelType_invalidEventFile_throwsIllegalValueException() throws Exception {
         JsonSerializableSchedule dataFromFile = JsonUtil.readJsonFile(INVALID_EVENT_FILE,
+                JsonSerializableSchedule.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidBlockedSlotFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableSchedule dataFromFile = JsonUtil.readJsonFile(INVALID_BLOCKEDSLOT_FILE,
+                JsonSerializableSchedule.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_clashingEventFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableSchedule dataFromFile = JsonUtil.readJsonFile(CLASHING_EVENT_FILE,
+                JsonSerializableSchedule.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_clashingBlockedSlotFile_throwsIllegalValueException() throws Exception {
+        JsonSerializableSchedule dataFromFile = JsonUtil.readJsonFile(CLASHING_BLOCKEDSLOT_FILE,
+                JsonSerializableSchedule.class).get();
+        assertThrows(IllegalValueException.class, dataFromFile::toModelType);
+    }
+
+    @Test
+    public void toModelType_eventClashesWithBlockedSlot_throwsIllegalValueException() throws Exception {
+        JsonSerializableSchedule dataFromFile = JsonUtil.readJsonFile(EVENT_CLASHES_WITH_BLOCKEDSLOT_FILE   ,
                 JsonSerializableSchedule.class).get();
         assertThrows(IllegalValueException.class, dataFromFile::toModelType);
     }
