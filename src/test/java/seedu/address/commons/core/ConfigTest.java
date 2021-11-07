c04112dcb93f2a1ece8a0331ef04f68f8b565bef
kaldius <quantf@gmail.com>
2021-11-07 15:43:59 +0800

    Add test cases to ConfigTest

--- src/test/java/seedu/address/commons/core/ConfigTest.java
+++ src/test/java/seedu/address/commons/core/ConfigTest.java
@@ -1,12 +1,25 @@
 package seedu.address.commons.core;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 
+import java.nio.file.Paths;
+import java.util.logging.Level;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ConfigTest {
+
+    @Test
+    public void getLogLevel() {
+        Config defaultConfig = new Config();
+        assertTrue(defaultConfig.getLogLevel().equals(Level.INFO));
+    }
+
+    @Test
+    public void gtUserPrefsFilePath() {
+        Config defaultConfig = new Config();
+        assertTrue(defaultConfig.getUserPrefsFilePath().equals(Paths.get("preferences.json")));
+    }
 
     @Test
     public void toString_defaultObject_stringReturned() {
@@ -23,5 +36,17 @@ public class ConfigTest {
         assertTrue(defaultConfig.equals(defaultConfig));
     }
 
+    @Test
+    public void equals_null_returnsFalse() {
+        Config defaultConfig = new Config();
+        assertFalse(defaultConfig.equals(null));
+    }
+
+    @Test
+    public void hashCodeMethod() {
+        Config configA = new Config();
+        Config configB = new Config();
+        assertTrue(configA.hashCode() == configB.hashCode());
+    }
 
 }
